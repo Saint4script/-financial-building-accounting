78eb489c5249a305ecb637eeffbe8f988c668dbd
Saint4script <saint.4.script@gmail.com>
2020-01-26 19:22:40 +0800

    checking empty fields

--- converter.py
+++ converter.py
@@ -6,7 +6,7 @@ from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication
 from zikkurat001 import Ui_MainWindow 
 from Tables import Ui_NewWindow
 
-
+#main window
 class mywindow(QtWidgets.QMainWindow):
     k = S = n = s = p1 = z = c = 0 
     def __init__(self):
@@ -14,8 +14,7 @@ class mywindow(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.Build.clicked.connect(self.BuildFunc)
-        self.ui.Test.clicked.connect(self.Trying)
-        
+        #self.ui.Test.clicked.connect(self.Trying) # для тестов
         self.ui.Total_area.editingFinished.connect(lambda field = self.ui.Total_area: self.CheckerForFields(field))
         self.ui.Apartments_amount.editingFinished.connect(lambda field = self.ui.Apartments_amount: self.CheckerForFields(field))
         self.ui.Average_area_of_apartments.editingFinished.connect(lambda field = self.ui.Average_area_of_apartments: self.CheckerForFields(field))
@@ -25,32 +24,53 @@ class mywindow(QtWidgets.QMainWindow):
         self.ui.Start_money.editingFinished.connect(lambda field = self.ui.Start_money: self.CheckerForFields(field))
         self.ui.Self_cost.editingFinished.connect(lambda field = self.ui.Self_cost: self.CheckerForFields(field))
         
-        
+       
     def BuildFunc(self):
-        if(self.ui.Project_cost.text() == ""):
-           tmp = int(self.ui.Self_cost.text())*int(self.ui.Total_area.text())
-           self.ui.Project_cost.setText(str(tmp))
-         
-        start_money = int(self.ui.Start_money.text())
-        self_cost = self.c = int(self.ui.Self_cost.text())
-        total_area = self.S = int(self.ui.Total_area.text())
-        self.n = int(self.ui.Apartments_amount.text())
-        self.s = int(self.ui.Average_area_of_apartments.text())
-        self.p1 = int(self.ui.Start_cost.text())
-        self.z = int(self.ui.Increasing_percentage.text())
-        
-        if(start_money < total_area * self_cost * 0.1):     
-            error_dialog = QtWidgets.QErrorMessage()
-            error_dialog.showMessage("Здесь.Не.Строят")
-            error_dialog.exec_()
-        else:
-             self.k = total_area * self_cost - start_money
-             application = newwindow(self)
-             application.show()
-             self.hide()
-
+        if(self.isnt_field_empty()): 
+            if(self.ui.Project_cost.text() == ""):
+               tmp = int(self.ui.Self_cost.text())*int(self.ui.Total_area.text())
+               self.ui.Project_cost.setText(str(tmp))
+            
+            start_money = int(self.ui.Start_money.text())
+            self_cost = self.c = int(self.ui.Self_cost.text())
+            total_area = self.S = int(self.ui.Total_area.text())
+            self.n = int(self.ui.Apartments_amount.text())
+            self.s = int(self.ui.Average_area_of_apartments.text())
+            self.p1 = int(self.ui.Start_cost.text())
+            self.z = int(self.ui.Increasing_percentage.text())
+            
+            if(start_money < total_area * self_cost * 0.1):     
+                error_dialog = QtWidgets.QErrorMessage()
+                error_dialog.showMessage("Здесь.Не.Строят")
+                error_dialog.exec_()
+            else:
+                 self.k = total_area * self_cost - start_money
+                 application = newwindow(self)
+                 application.show()
+                 self.hide()
+    
+    
+    def isnt_field_empty(self):
+        lineEdits = self.ui.centralwidget.findChildren(QtWidgets.QLineEdit)
+        checker = True
+        for lineEdit in lineEdits:
+            if(lineEdit.text() == ""):
+                if (lineEdit != self.ui.Project_cost):
+                    lineEdit.setStyleSheet("background-color:yellow;")
+                    checker = False
+            else:
+                lineEdit.setStyleSheet("background-color:none;")
+        self.ui.Project_cost.setStyleSheet("background-color:none;")
+        if(not checker):
+            warning = QtWidgets.QMessageBox()
+            warning.setWindowTitle("Empty field")
+            warning.setText("Fill all the fields")
+            warning.setIcon(QtWidgets.QMessageBox.Critical)
+            warning.exec_() 
+        return checker
+                
+                
     def CheckerForFields(self, field): #Проверка заполнения полей, чтобы не вводили что-то кроме чисел
-
         if(field.text() != ""): #Если не писать это условие, прога вообще не запускается _-_
             try:
                 int(field.text())
@@ -60,10 +80,13 @@ class mywindow(QtWidgets.QMainWindow):
                 error.exec_()
                 field.setText("")
     
+    
+    '''
     def Trying(self):
         if(self.ui.Total_area.text() != "" and self.ui.Self_cost.text() != ""):
             tmp = int(self.ui.Self_cost.text())*int(self.ui.Total_area.text())
             self.ui.Project_cost.setText(str(tmp))
+    '''
         
         
 #Это теперь дочерний класс класса mywindow
@@ -72,9 +95,9 @@ class newwindow(QtWidgets.QMainWindow):
         super(newwindow, self).__init__(parent)
         self.ui = Ui_NewWindow()
         self.ui.setupUi(self)
-        self.ui.pushButton.clicked.connect(self.bp)
+        self.ui.pushButton.clicked.connect(self._exit)
     
-    def bp(self):
+    def _exit(self):
         self.parent().show()
         self.close()
         
